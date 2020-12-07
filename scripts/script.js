93c6ba66a30439c4747b6ba7102b8369c32e27d9
mereltje99 <32360595+mereltje99@users.noreply.github.com>
2020-12-07 16:47:37 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 // JavaScript Document
+var menubutton =document.querySelector("div[role='menu']");
+
+menubutton.addEventListener("click",openmenu);
+
+function openmenu(){
+	var hetmenu =document.querySelector("header nav");
+	hetmenu.classList.toggle("open");
+
+}
